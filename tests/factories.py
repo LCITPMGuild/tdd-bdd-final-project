cf0cf233dadfc86a905760da325529f2b541f6f8
PMGuild <138247463+LCITPMGuild@users.noreply.github.com>
2023-11-19 01:21:38 -0500

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -31,4 +31,15 @@ class ProductFactory(factory.Factory):
         model = Product
 
     id = factory.Sequence(lambda n: n)
-   ## Add code to create Fake Products 
+        def test_read_a_product(self):
+        """It should Read a Product"""
+        product = ProductFactory()
+        product.id = None
+        product.create()
+        self.assertIsNotNone(product.id)
+        # Fetch it back
+        found_product = Product.find(product.id)
+        self.assertEqual(found_product.id, product.id)
+        self.assertEqual(found_product.name, product.name)
+        self.assertEqual(found_product.description, product.description)
+        self.assertEqual(found_product.price, product.price)
